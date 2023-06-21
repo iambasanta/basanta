fddbcb94a5febd18985cc3596e5821ec5a95d343
iambasanta <sangpangbasanta@gmail.com>
2023-06-21 20:09:50 +0545

    Initial version

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+console.log(`
+Basanta Rai
+
+I am Basanta Rai, a passionate BSc.CSIT student from Nepal, captivated by backend development and finding immense satisfaction in unraveling the complexities complexities of coding and constructing resilient systems. The guitar serves as a special creative outlet, alongside my academic pursuits. Driven by curiosity, I strive for excellence in my studies while nurturing my interests in backend development, music, and fitness.
+
+Github: https://github.com/iambasanta
+Website: https://www.basantarai.com.np
+
+`);
