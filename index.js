ffc616269d5e9e212279431077a3998d55210a7a
iambasanta <sangpangbasanta@gmail.com>
2023-06-21 20:42:31 +0545

    Don't clear console

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
 
-const clearScreen = () =>
-  process.stdout.write(
-    process.platform === "win32" ? "\x1B[2J\x1B[0f" : "\x1B[2J\x1B[3J\x1B[H"
-  );
-
-clearScreen();
-
 console.log(`
+   ___
+  {o,o}
+  |)__)
+  -"-"-
+
 Basanta Rai
 
 I am Basanta Rai, a passionate BSc.CSIT student from Nepal, captivated by backend development and finding immense satisfaction in unraveling the complexities complexities of coding and constructing resilient systems. The guitar serves as a special creative outlet, alongside my academic pursuits. Driven by curiosity, I strive for excellence in my studies while nurturing my interests in backend development, music, and fitness.
