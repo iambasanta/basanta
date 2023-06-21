418dbde898ee54f3167e5c38b20f66b71de0329a
iambasanta <sangpangbasanta@gmail.com>
2023-06-21 20:27:34 +0545

    Clear screen

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 
+const clearScreen = () =>
+  process.stdout.write(
+    process.platform === "win32" ? "\x1B[2J\x1B[0f" : "\x1B[2J\x1B[3J\x1B[H"
+  );
+
+clearScreen();
+
 console.log(`
 Basanta Rai
 
